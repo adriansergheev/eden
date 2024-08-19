bc39c5bb89f8d3aacb6aa850df61bccc78c6e249
Adrian Sergheev <sergheevdev@icloud.com>
2024-08-19 16:10:52 +0200

    tca

--- modules/Package.swift
+++ modules/Package.swift
@@ -10,10 +10,16 @@ let package = Package(
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(name: "Content", targets: ["Content"]),
   ],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.13.0"),
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
-    .target(name: "Content"),
+    .target(
+      name: "Content",
+      dependencies: [.product(name: "ComposableArchitecture", package: "swift-composable-architecture")]
+    ),
     .testTarget(name: "ContentTests", dependencies: ["Content"]),
   ]
 )
--- modules/Sources/Content/ContentView.swift
+++ modules/Sources/Content/ContentView.swift
@@ -1,9 +1,10 @@
 import SwiftUI
+import ComposableArchitecture
 
 public struct ContentView: View {
 
   public init() {}
-  
+
   public var body: some View {
     VStack {
       Image(systemName: "globe")
